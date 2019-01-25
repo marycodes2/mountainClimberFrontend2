f41b5968af5c1344c430076b8377695dea8374dd
marycodes2 <mbeliveau92@gmail.com>
2019-01-25 16:14:18 -0500

    first commit

---
+++ src/Containers/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import FirstScreen from '../Components/FirstScreen'
+
+class App extends Component {
+  // componenDidMount() {
+  //   return <FirstScreen />
+  // }
+  render() {
+    return (
+      <div className="App">
+        <FirstScreen />
+      </div>
+    );
+  }
+}
+
+export default App;
